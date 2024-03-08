ced997f42a92271a8c10db434ef19aff2fc04e7d
COREY FSDI-111 <Corey.Jonhson@sdgku.edu>
2024-03-07 23:05:14 -0500

    FSDI-111 class3 3/7/24

---
+++ mc1.md
@@ -0,0 +1,19 @@
+# Mini Challenge 1
+
+## SQL primer
+
+### Acceptance Criteria
+
+1. Create the following records:
+1.1. Robert Martin whose hobbies are "writing and reading books"
+1.2. John Doe whose hobbies are "playing golf and watching TV"
+1.3. Jane Doe whose hobbies are "Playing tennis and skiing"
+2. Deactivate John Doe's account by updating the "online" column to 0 for his record.
+3. Delete Robert Martin's record from the table.
+4. Execute a select statement that displays all records in this order: last_name, first_name, hobbies
+
+```
+Hint:
+You can change the order of columns like so:
+SELECT last_name, first_name FROM...
+```
---
+++ task_mgr/app/routes.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from app.database import task
+
+
+app = Flask(__name__)
+
+@app.get("/task")
+def get_all_task():
+    task_list = task.scan()
+    out = {
+        "tasks": task_list,
+        "ok": True
+    }
+    return out
+
+@app.get("/tasks/<int:pk>")
+def get_single_task(pk):
+    single_task = task.select_by_id(pk)
+    out = {
+        "task": single_task,
+        "ok": True
+    }
+    return out
+
+@app.put("/tasks/<int:pk>")
+def update_task(pk):
+    task_data = request.json
+    task.update_by_id(pk, task_data)
+    return "", 204
+
+@app.delete("/task/<int:pk>")
+def delete_task(pk):
+    task.delete(pk)
+    return "", 204
